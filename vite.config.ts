dc005dd2bbb14bc5269b35aafb8136a5b3e830b6
faruqeclypst <faruq.blogger@gmail.com>
2025-09-12 02:14:06 +0700

    deploy vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     // kalau host tunnelnya tetap
     allowedHosts: [
       'localhost',
-      'reno-statewide-pharmacies-conflict.trycloudflare.com'
+      'https://accepted-du-valuable-automatic.trycloudflare.com/'
     ],
 
     // kalau sering berubah (pakai ngrok/trycloudflare random subdomain)
